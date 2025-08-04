c2be502fc433fd164a93181ba70f035c8dd761de
digitalstars30 <159788811+digitalstars30@users.noreply.github.com>
2025-08-04 10:58:04 -0700

    Update text_tel.js

--- text_tel.js
+++ text_tel.js
@@ -1,4 +1,4 @@
-window.defaultNumber = '+1-000-000-0000';
+window.defaultNumber = '+1-802-829-9564';
 window.defaultText = 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Apple Support +1-000-000-0000 to Freeze it!';
 window.text = {
     'xhamster.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-000-000-0000 to unlock it!',
